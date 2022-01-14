f3440fd94e0387e99a59d9411fe19446a87f8b7c
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-01-14 18:54:48 +0000

    CodeBlock component: adjust style

--- src/dsmsite/ui/components/CodeBlock.js
+++ src/dsmsite/ui/components/CodeBlock.js
@@ -5,7 +5,7 @@ import { PSmall } from '../../../hb_ui/elements/typography'
 
 const CodeBlock = ({ code }) => {
   return (
-    <pre tw='mb-4 overflow-x-scroll text-sm text-secondary rounded border border-secondary bg-grey-light-200 dark:bg-grey-dark-400 p-4'>
+    <pre tw='mb-4 overflow-x-scroll lg:overflow-x-visible text-sm text-secondary rounded border border-secondary bg-grey-light-200 dark:bg-grey-dark-700 p-4'>
       {code}
     </pre>
   )
