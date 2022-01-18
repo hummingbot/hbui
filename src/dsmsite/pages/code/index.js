c3af3dbab9370bba8454c6287afe9fef45678254
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-01-18 17:51:45 +0000

    Add Code overview page

--- src/dsmsite/pages/code/index.js
+++ src/dsmsite/pages/code/index.js
@@ -1,15 +1,47 @@
 import React from 'react'
-import { P } from '../../../hb_ui/elements/typography'
+import { Body, P, H5, Bold } from '../../../hb_ui/elements/typography'
+import { Separator } from '../../../hb_ui/elements/layout'
 import { PageRoot, ShortHero, MainContent } from '../../ui/elements/layout'
+import twinMacroLogo from './images/twin-macro.svg'
+import tailwindCssLogo from './images/tailwind.svg'
+import emotionLogo from './images/emotion.png'
 
 function Code() {
   return (
     <PageRoot>
       <ShortHero
         title='Code'
+        subTitle='The code foundations of the HB design system'
       />
       <MainContent>
-        <P>Code content</P>
+        <H5>Overview</H5>
+        <br />
+        <P>The HB design system provides a ready-to-use code framework which makes the UI development quick and consistent.</P>
+        <br />
+        <P>To avoid re-inventing the wheel, the HB design system is built upon existing 3rd-party libraries to leverage the support of active communities that evolve independently and provide well-maintained documentation.</P>
+        <br />
+        <br />
+        <Separator />
+        <br />
+        <br />
+        <H5>The three pillars of this framework:</H5>
+        <br />
+        <br />
+        <img src={tailwindCssLogo} alt='Tailwind CSS' />
+        <br />
+        <P><Bold>Tailwind CSS</Bold> is used as the CSS foundation upon which the design system is built. Tailwind CSS is a powerful and flexible CSS framework that provides an excellent set of standards to ensure consistency across all styles and has a large active community as well as a great documentation site.</P>
+        <br />
+        <br />
+        <img src={twinMacroLogo} alt='twin.macro' />
+        <br />
+        <P><Bold>Twin.macro</Bold> blends the magic of Tailwind with the flexibility of css-in-js, making it the ideal solutin to use Tailwind CSS in React applications.</P>
+        <br />
+        <br />
+        <img src={emotionLogo} alt='Emotion.js' style={{maxHeight: '100px'}} />
+        <br />
+        <P><Bold>Emotion.js</Bold> is the second most popular css-in-js library available. Our choice of Emotion.js is based on the fact that it is smaller and around 20% faster then styled-components. As of now, there are no features unique to styled-components that justify using styled-components instead of Emotion.js. We also considered using Stitches.js which is even faster, but still has a small community compared to that of Emotion.js.</P>
+        <br />
+        <br />
       </MainContent>
     </PageRoot>
   )
