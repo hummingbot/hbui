0646cc5a419155f41a9f43b8c0d65e2de12f9836
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-01-28 17:20:39 +0000

    Cleanup dark light mode setup

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,59 +5,34 @@ import colors from '../hb_ui/constants/colors'
 
 const customStyles = css`
   .light {
-    --color-primary: ${colors.green_primary};
-    --color-info: ${colors.blue_bright};
-    --color-warning: ${colors.orange};
-    --color-danger: ${colors.red};
-
-    --bg-brand: ${colors.green_primary};
     --bg-primary: #ffffff;
     --bg-secondary: ${colors.greyscale_light_100};
     --bg-tertiary: ${colors.greyscale_light_200};
     --bg-quaternary: ${colors.greyscale_light_400};
 
-    --border-brand: ${colors.green_primary};
     --border-primary: ${colors.greyscale_light_200};
     --border-secondary: ${colors.greyscale_light_300};
     --border-tertiary: ${colors.greyscale_light_400};
 
-    --text-brand: ${colors.green_primary};
     --text-primary: ${colors.greyscale_dark_700};
     --text-secondary: ${colors.greyscale_dark_600};
-    --text-dim: ${colors.greyscale_light_600};
     --text-tertiary: ${colors.greyscale_dark_400};
-
-    --fill-primary: ${colors.greyscale_dark_700};
-    --fill-secondary: ${colors.greyscale_dark_600};
-    --fill-tertiary: ${colors.greyscale_dark_400};
+    --text-quaternary: ${colors.greyscale_light_600};
   }
   .dark {
-    --color-primary: ${colors.green_primary};
-    --color-info: ${colors.blue_bright};
-    --color-warning: ${colors.orange};
-    --color-danger: ${colors.red};
-
-    --bg-brand: ${colors.green_primary};
     --bg-primary: ${colors.greyscale_dark_700};
     --bg-secondary: ${colors.greyscale_dark_600};
     --bg-tertiary: ${colors.greyscale_dark_500};
     --bg-quaternary: ${colors.greyscale_dark_300};
 
-    --border-brand: ${colors.green_primary};
     --border-primary: ${colors.greyscale_dark_400};
     --border-secondary: ${colors.greyscale_dark_200};
     --border-tertiary: ${colors.greyscale_dark_100};
 
-    --text-brand: ${colors.green_primary};
     --text-primary: #ffffff;
     --text-secondary: ${colors.greyscale_light_200};
     --text-tertiary: ${colors.greyscale_light_400};
-    --text-dim: ${colors.greyscale_light_600};
-
-    --fill-brand: ${colors.green_primary};
-    --fill-primary: #ffffff;
-    --fill-secondary: ${colors.greyscale_light_200};
-    --fill-tertiary: ${colors.greyscale_light_400};
+    --text-quaternary: ${colors.greyscale_light_600};
   }
   body {
     -webkit-tap-highlight-color: ${colors.yellow};
