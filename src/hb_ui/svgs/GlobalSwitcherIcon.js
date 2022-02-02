80678bce40a86370a698dacad78c7e48da2236f4
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-02-02 17:37:25 +0000

    Rename sgv icon

--- src/hb_ui/svgs/GlobalSwitcherIcon.js
+++ src/hb_ui/svgs/GlobalSwitcherIcon.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function HummingbotLogo() {
+function GlobalSwitcherIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -22,4 +22,4 @@ function HummingbotLogo() {
   )
 }
 
-export default HummingbotLogo
+export default GlobalSwitcherIcon
