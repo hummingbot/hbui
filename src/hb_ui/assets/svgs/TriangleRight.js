037a7348ee549b1dcc4b3eda83453f54866509e8
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-04-18 02:17:16 +0100

    Add smart color to svgs

--- src/hb_ui/assets/svgs/TriangleRight.js
+++ src/hb_ui/assets/svgs/TriangleRight.js
@@ -6,11 +6,10 @@ function TriangleRight() {
       xmlns="http://www.w3.org/2000/svg"
       width="4"
       height="8"
-      tw="fill-current"
+      tw="fill-current text-quintenary"
       viewBox="0 0 4 8"
     >
       <path
-        fill="inherit"
         d="M.932.275a.466.466 0 00-.664-.02.466.466 0 00-.02.665l2.5 2.656-2.5 2.637a.466.466 0 00.02.664.466.466 0 00.664-.02l2.812-2.969a.52.52 0 00.118-.312.53.53 0 00-.118-.332L.932.275z"
       ></path>
     </svg>
