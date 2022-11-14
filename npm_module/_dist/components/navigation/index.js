df89f8fc4a77b8df92c6ab427e1ced9fda054cc4
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-11-14 20:06:16 +0000

    Clean up

--- npm_module/_dist/components/navigation/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-exports.__esModule = true;
-exports.NavBar = exports.BreadcrumbsBar = void 0;
-var _BreadcrumbsBar = _interopRequireDefault(require("./BreadcrumbsBar"));
-exports.BreadcrumbsBar = _BreadcrumbsBar["default"];
-var _NavBar = _interopRequireDefault(require("./NavBar"));
-exports.NavBar = _NavBar["default"];
--- npm_module/_dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports["default"] = void 0;
-var warning = 'HBUI: do no import at the root';
-var _default = warning;
-exports["default"] = _default;
