cb5e6e1a0cf3d788154c1b5548c048f1495a88fa
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-11-29 14:55:29 +0000

    Fix combobox on light mode

--- hbui/components/combobox/index.js
+++ hbui/components/combobox/index.js
@@ -21,7 +21,7 @@ export default function ComboBox({items, selected, setSelected, ...props}) {
       <div tw="relative">
         <div tw="relative w-full text-left rounded-lg cursor-default focus:outline-none sm:text-sm overflow-hidden">
           <Combobox.Input
-            tw="w-full bg-input font-medium border-none outline-none py-2 pl-3 pr-10 text-sm leading-5 text-secondary"
+            tw="w-full bg-grey-light-scale-300 dark:bg-input font-medium border-none outline-none py-2 pl-3 pr-10 text-sm leading-5 text-secondary"
             displayValue={(item) => item.label}
             onChange={(event) => setQuery(event.target.value)}
           />
@@ -32,7 +32,7 @@ export default function ComboBox({items, selected, setSelected, ...props}) {
             />
           </Combobox.Button>
         </div>
-        <Combobox.Options tw="absolute w-full py-1 mt-1 overflow-auto text-base bg-body rounded-md shadow-lg max-h-60 focus:outline-none sm:text-sm z-50">
+        <Combobox.Options tw="absolute w-full py-1 mt-1 overflow-auto text-base bg-window border border-window rounded-md shadow-lg max-h-60 focus:outline-none sm:text-sm z-50">
           {filteredItems.length === 0 && query !== '' ? (
             <div tw="cursor-default select-none relative py-2 px-4 text-primary">
               Nothing found.
@@ -74,8 +74,8 @@ const comboBoxStyles = {}
 comboBoxStyles[true] = tw`cursor-default select-none relative py-2 pl-10 pr-4 text-tertiary`
 comboBoxStyles[false] = tw`cursor-default select-none relative py-2 pl-10 pr-4 text-secondary`
 const spanStyles = {}
-spanStyles[true] = tw`block truncate font-medium px-4 pl-10 py-2 hover:bg-input hover:cursor-pointer`
-spanStyles[false] = tw`block truncate font-normal px-4 pl-10 py-2 hover:bg-input hover:cursor-pointer`
+spanStyles[true] = tw`block truncate font-medium px-4 pl-10 py-2 hover:cursor-pointer hover:bg-grey-light-scale-300 dark:(hover:bg-grey-dark-scale-300) focus:hover:bg-red`
+spanStyles[false] = tw`block truncate font-normal px-4 pl-10 py-2 hover:cursor-pointer hover:bg-grey-light-scale-300 dark:(hover:bg-grey-dark-scale-300) focus:hover:bg-red`
 const spanBStyles = {}
 spanBStyles[true] = tw`absolute inset-y-0 left-0 top-0 flex items-center pl-3 text-green text-current`
 spanBStyles[false] = tw`absolute inset-y-0 left-0 top-0 flex items-center pl-3 text-orange text-current`
