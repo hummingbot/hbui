d1eafdddd8a9f27576ea0d41ab84990e59ae0a6f
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-12-01 18:01:13 +0000

    Combobox: adjust styles

--- hbui/components/combobox/index.js
+++ hbui/components/combobox/index.js
@@ -48,7 +48,7 @@ export default function ComboBox({items, selected, setSelected, placeholderName,
     <Combobox value={selected} onChange={setSelected} {...props}>
       <div tw="relative">
 
-        <div tw="relative w-full text-left rounded-lg cursor-default focus:outline-none sm:text-sm overflow-hidden">
+        <div tw="relative w-full text-left rounded cursor-default focus:outline-none sm:text-sm overflow-hidden">
           <Combobox.Input
             tw="w-full bg-window font-medium border border-window outline-none py-2 pl-3 pr-10 text-sm leading-5 text-secondary"
             placeholder={getPlaceholder()}
@@ -105,7 +105,7 @@ const comboBoxStyles = {}
 comboBoxStyles[true] = tw`cursor-default select-none relative py-2 pl-10 pr-4 text-tertiary`
 comboBoxStyles[false] = tw`cursor-default select-none relative py-2 pl-10 pr-4 text-secondary`
 const spanStyles = {}
-spanStyles[true] = tw`block truncate font-medium px-4 pl-10 py-2 hover:cursor-pointer bg-grey-light-scale-300 dark:(bg-grey-dark-scale-300)`
+spanStyles[true] = tw`block truncate font-medium px-4 pl-10 py-2 bg-grey-light-scale-500/10 hover:cursor-pointer`
 spanStyles[false] = tw`block truncate font-normal px-4 pl-10 py-2 hover:cursor-pointer`
 const spanBStyles = {}
 spanBStyles[true] = tw`absolute inset-y-0 left-0 top-0 flex items-center pl-3 text-green text-current`
