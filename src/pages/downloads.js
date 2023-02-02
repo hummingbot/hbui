b64fcc45dd8e7cddf2e535029358bbb0de468b1c
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-02-02 20:24:18 +0000

    Revise downloads page, add labels and utility typefaces

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -17,23 +17,23 @@ const DesignTokens = () => (
       <br />
       <br />
       <PLarge>In addition to the assets provided here, there are additional design resources available in our <a href='https://www.figma.com/file/hf03Mn2cecwA1ioUxlHg9P/Hummingbot-Design-System-1.3' target='_blank' rel="noreferrer">Figma workspace</a>.</PLarge>
-      <PLarge>In addition to the assets provided here, there are additional design resources available in our <a href='https://www.figma.com/file/hf03Mn2cecwA1ioUxlHg9P/Hummingbot-Design-System-1.3' target='_blank' rel="noreferrer">Figma workspace</a>.</PLarge>
       <br />
       <br />
       <H5 isBold>Logo</H5>
       <Separator />
       <br />
       <br />
-      <div tw='w-full grid grid-cols-1 xl:grid-cols-2 gap-8'>
+      <div>
         <DownloadCard
           title='CoinAlpha Logotype'
-          updatedAt='15.1.2022'
+          updatedAt='Feb 2023'
           downloadLink='/assets/downloads/coinalpha/coinalpha_logotype.zip'
           image='/assets/downloads_page/coinalpha_logotype.png'
         />
+        <br />
         <DownloadCard
           title='Hummingbot Logotype'
-          updatedAt='15.1.2022'
+          updatedAt='Feb 2023'
           downloadLink='/assets/downloads/hummingbot/hummingbot_logotype.zip'
           image='/assets/downloads_page/hummingbot_logotype.png'
         />
@@ -47,12 +47,47 @@ const DesignTokens = () => (
       <Separator />
       <br />
       <br />
-      <div tw='w-full grid grid-cols-1 xl:grid-cols-2 gap-8'>
+      <div>
         <DownloadCard
-          title='Satoshi: The main CoinAlpha typeface'
-          updatedAt='15.1.2022'
+          title='Main typeface'
+          updatedAt='Feb 2023'
           downloadLink='/assets/downloads/typography/satoshi.zip'
           image='/assets/downloads_page/brand_typeface.png'
+        />
+      </div>
+      <br />
+      <div>
+        <DownloadCard
+          title='Typeface for small labels'
+          updatedAt='Feb 2023'
+          downloadLink='/assets/downloads/typography/inter.zip'
+          image='/assets/downloads_page/labels_typeface.png'
+        />
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <H5 isBold>Utility typefaces</H5>
+      <Separator />
+      <br />
+      <br />
+      <div>
+        <DownloadCard
+          title='Annotations'
+          updatedAt='Feb 2023'
+          downloadLink='/assets/downloads/typography/indie_flower.zip'
+          image='/assets/downloads_page/annotations_typeface.png'
+        />
+      </div>
+      <br />
+      <div>
+        <DownloadCard
+          title='Arrows & Utilities'
+          updatedAt='Feb 2023'
+          downloadLink='/assets/downloads/typography/pw_new_arrows.zip'
+          image='/assets/downloads_page/utilities_typeface.png'
         />
       </div>
       <br />
@@ -69,7 +104,7 @@ const DownloadCard = ({title, updatedAt, downloadLink, image}) => {
   return (
     <DownloadCardElement tw='flex border-2 border-window rounded'>
       <img src={image} alt={title} tw='max-h-40 lg:max-h-60 border-r border-window' />
-      <div tw='p-4 lg:p-8 grid justify-between content-between '>
+      <div tw='py-2 px-3 lg:(py-4 px-6) grid justify-between content-between '>
         <div>
           <H6 isMedium tw='mb-2'>{title}</H6>
           <P tw='text-tertiary'>Updated at {updatedAt}</P>
