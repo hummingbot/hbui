5966a1ef7039394ee5ee321df56c61a0b8fa370a
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-02-07 16:06:44 +0000

    Update NPM module to 1.8.20

--- hbui/system/GlobalStyles.js
+++ hbui/system/GlobalStyles.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { GlobalStyles as BaseStyles } from 'twin.macro'
 import HBUIColors from './HBUIColors'
 
-import '@hummingbot/hbui/assets/fonts/inter/inter.css'
-import '@hummingbot/hbui/assets/fonts/satoshi/satoshi.css'
-import '@hummingbot/hbui/assets/fonts/sfmono/sfmono.css'
+// load fonts
+import '../assets/fonts/inter/inter.css'
+import '../assets/fonts/satoshi/satoshi.css'
+import '../assets/fonts/sfmono/sfmono.css'
 
 const GlobalStyles = () => (
   <>
