235c204fd7f83c6b1c09fecaf83d36b0a0dea7e3
Antonio Brandao <antoniobrandaodesign@gmail.com>
2023-02-07 16:04:12 +0000

    Re-add font loading to GlobalStyles

--- hbui/system/GlobalStyles.js
+++ hbui/system/GlobalStyles.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { GlobalStyles as BaseStyles } from 'twin.macro'
 import HBUIColors from './HBUIColors'
 
+import '@hummingbot/hbui/assets/fonts/inter/inter.css'
+import '@hummingbot/hbui/assets/fonts/satoshi/satoshi.css'
+import '@hummingbot/hbui/assets/fonts/sfmono/sfmono.css'
+
 const GlobalStyles = () => (
   <>
     <BaseStyles />
